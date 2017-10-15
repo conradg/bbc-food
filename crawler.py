62ed283cc436df480eec90e2e28ddfbca54a7409
Conrad Godfrey <conradjgodfrey@gmail.com>
2017-10-15 22:32:13 +0100

    Change crawler settings
    can now get almost all recipes by searching for "t"

--- crawler.py
+++ crawler.py
@@ -6,12 +6,11 @@ base_url = 'http://www.bbc.co.uk'
 search_url = 'http://www.bbc.co.uk/food/recipes/search'
 
 query_params = {
-  "page"      :"1",
-  "keywords"  :"",
-  "x"         :"17",
-  "y"         :"15",
-  "diets[0]"  :"vegetarian",
-  "sortBy"    :"lastModified",
+    "page": "1",
+    "keywords": "t",
+    "x": "0",
+    "y": "0",
+    "sortBy": "lastModified",
 }
 
 cookie = 'BBC-UID=1557a39a2f77f15cadde8197d113d75c3c7da8fed31896233b6b54639ea804440Mozilla/5.0%20(Windows%20NT%2010.0%3b%20WOW64)%20AppleWebKit/537.36%20(KHTML%2c%20like%20Gecko)%20Chrome/50.0.2661.94%20Safari/5; ckns_policy=111; ckns_policy_exp=1495018131230; s1=220.181.573AF71D0006CB00B052C72985; _cb_ls=1; _chartbeat2=CLVHREBFMBlYBCcyNy.1463482132006.1463482146992.1; ecos.dt=1463482147068; _chartbeat5=879,294,%2Ffood%2Frecipes%2Fsearch,http%3A%2F%2Fwww.bbc.co.uk%2Ffood%2Frecipes%2Fsearch%3Fpage%3D2%26keywords%3D%26x%3D18%26y%3D9%26diets%255B0%255D%3Dvegetarian%26sortBy%3DlastModified,D44zN8B61JnUBQdBOoCzint3nBBUi,*%5B%40id%3D\'column%2D1\'%5D%2Fdiv%5B2%5D%2Fol%5B1%5D%2Fli%5B7%5D%2Fa%5B1%5D,c,kh7jnufzSOFCY8GMrJn4Dy6vUo'
